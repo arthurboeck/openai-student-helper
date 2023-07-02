6cc20226015d87fce2432f694b3ca08baaeaf49e
Arthur Boeck <arthurguterres@outlook.com>
2023-07-02 20:11:14 -0300

    rename QuestionsAndAnswersController unit tests

---
+++ src/test/kotlin/com/openai/student/helper/controller/QuestionsAndAnswersControllerTest.kt
@@ -0,0 +1,98 @@
+package com.openai.student.helper.controller
+
+import com.openai.student.helper.BaseControllerTest
+import com.openai.student.helper.infra.exceptions.UnauthorizedException
+import com.openai.student.helper.service.questionsandanswers.IQuestionsAndAnswersService
+import org.hamcrest.Matchers.containsString
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.doThrow
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(controllers = [QuestionsAndAnswersController::class])
+class QuestionsAndAnswersControllerTest : BaseControllerTest() {
+
+    private val CONTENT_SUGGESTION_TEXT = "/v1/open-ai/student-helper/question-answer/text"
+    private val CONTENT_SUGGESTION_JSON = "/v1/open-ai/student-helper/question-answer/json"
+    
+    @MockBean
+    private lateinit var iTopicQuestionsService: IQuestionsAndAnswersService
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun `Must Returns Bad Request on Questions TEXT when topic is empty`(topic: String) {
+        validateBadRequest(CONTENT_SUGGESTION_TEXT, topic)
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.error", containsString(CONSTRAINT_VIOLATION_EXCEPTION_MSG)))
+            .andExpect(jsonPath("$.error", containsString(TOPIC_REQUIRED_MSG)))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " "])
+    fun `Must Returns Bad Request on Questions JSON when topic is empty`(topic: String) {
+        validateBadRequest(CONTENT_SUGGESTION_JSON, topic)
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.error", containsString(CONSTRAINT_VIOLATION_EXCEPTION_MSG)))
+            .andExpect(jsonPath("$.error", containsString(TOPIC_REQUIRED_MSG)))
+    }
+
+    @Test
+    fun `Must Returns Sucess on Questions TEXT when topic is sent`(){
+        validateSuccessRequest(CONTENT_SUGGESTION_TEXT)
+            .andExpect(status().isOk)
+            .andExpect(content().string("Bananas são frutas"))
+    }
+
+    @Test
+    fun `Must Returns Sucess on Questions JSON when topic is sent`(){
+        validateSuccessRequest(CONTENT_SUGGESTION_JSON)
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.message", `is`("Bananas são frutas")))
+    }
+
+    @Test
+    fun `Must Returns Unauthorized on Questions TEXT when Open AI Key is Invalid`(){
+        validateUnauthorizedRequest(CONTENT_SUGGESTION_TEXT)
+            .andExpect(status().isUnauthorized)
+            .andExpect(jsonPath("$.error", containsString(UNAUTHORIZED_EXCEPTION_MSG)))
+            .andExpect(jsonPath("$.error", containsString(UNAUTHORIZED_INVALID_KEY_MSG)))
+    }
+    @Test
+    fun `Must Returns Unauthorized on Questions JSON when Open AI Key is Invalid`(){
+        validateUnauthorizedRequest(CONTENT_SUGGESTION_JSON)
+            .andExpect(status().isUnauthorized)
+            .andExpect(jsonPath("$.error", containsString(UNAUTHORIZED_EXCEPTION_MSG)))
+            .andExpect(jsonPath("$.error", containsString(UNAUTHORIZED_INVALID_KEY_MSG)))
+    }
+
+    private fun validateBadRequest(route: String,topic: String): ResultActions {
+        return mvc.perform(
+            get(route)
+                .queryParam("topic", topic)
+        )
+    }
+    private fun validateSuccessRequest(route: String): ResultActions {
+        doReturn("Bananas são frutas").`when`(iTopicQuestionsService).getQuestionsAndAnswers("bananas")
+
+        return mvc.perform(
+            get(route)
+                .queryParam("topic", "bananas")
+        )
+    }
+    private fun validateUnauthorizedRequest(route: String): ResultActions {
+        doThrow(UnauthorizedException("User Unauthorized - Invalid Open AI key"))
+            .`when`(iTopicQuestionsService).getQuestionsAndAnswers("bananas")
+
+        return mvc.perform(
+            get(route)
+                .queryParam("topic", "bananas")
+        )
+    }
+}
