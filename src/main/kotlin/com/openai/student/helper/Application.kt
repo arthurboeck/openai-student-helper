651dc7417c08a5740970f540fea97cc54960fefe
Arthur Boeck <arthurguterres@outlook.com>
2023-07-02 18:30:59 -0300

    rename application

--- src/main/kotlin/com/openai/student/helper/Application.kt
+++ src/main/kotlin/com/openai/student/helper/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TaleGeneratorApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<TaleGeneratorApplication>(*args)
+	runApplication<Application>(*args)
 }
