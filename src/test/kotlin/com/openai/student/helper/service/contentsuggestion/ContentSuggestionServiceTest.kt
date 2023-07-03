a5b82627c4e4310a4f7b07ff77bf4e9b55421ad1
Arthur Boeck <arthurguterres@outlook.com>
2023-07-02 21:25:09 -0300

    add unit tests for services

---
+++ src/test/kotlin/com/openai/student/helper/service/contentsuggestion/ContentSuggestionServiceTest.kt
@@ -0,0 +1,40 @@
+package com.openai.student.helper.service.contentsuggestion
+
+import com.openai.student.helper.BaseUnitTest
+import com.openai.student.helper.infra.client.IOpenAIClient
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.boot.test.mock.mockito.MockBean
+
+@ExtendWith(MockitoExtension::class)
+class ContentSuggestionServiceTest : BaseUnitTest() {
+
+    @MockBean
+    lateinit var openAIClient: IOpenAIClient
+    lateinit var service: ContentSuggestionService
+
+    @BeforeEach
+    fun setUp() {
+        service = ContentSuggestionService(openAIClient)
+    }
+
+    @Test
+    fun `Must return suggested content`() {
+        val topic = "Math"
+        val expectedResponse = "Suggested content"
+
+        val context = "Você é um auxiliar para estudos."
+        val question = "Me sugira três pontos chave para estudar sobre: \"$topic\""
+
+        `when`(openAIClient.integrateChatGpt(context, question)).thenReturn(expectedResponse)
+
+        val result = service.getContentSuggestion(topic)
+
+        assertThat(expectedResponse, `is`(result))
+    }
+}
