d7f226ca8488b135b26a52592cd562d2261f79e4
Arthur Boeck <arthurguterres@outlook.com>
2023-07-03 22:12:02 -0300

    add handler to infra

--- src/main/kotlin/com/openai/student/helper/infra/handler/ExceptionHandler.kt
+++ src/main/kotlin/com/openai/student/helper/infra/handler/ExceptionHandler.kt
@@ -1,4 +1,4 @@
-package com.openai.student.helper.handler
+package com.openai.student.helper.infra.handler
 
 import com.openai.student.helper.infra.exceptions.InvalidFileTypeException
 import com.openai.student.helper.infra.exceptions.NotFoundException
